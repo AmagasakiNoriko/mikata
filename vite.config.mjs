0ec879b75c3cb51b4a234812e5423db3d04b53fc
AmagasakiNoriko <ochatennis@yahoo.co.jp>
2024-10-19 21:40:29 +0900

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ base:'https://github.com/AmagasakiNoriko/mikata'
   plugins: [
     VueRouter(),
     Vue({
